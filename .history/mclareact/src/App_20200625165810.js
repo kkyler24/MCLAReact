84ca1201bdd6c923a377b65fc3226cd3fa725da2
Ky Kyler <ky.kyler1@gmail.com>
2020-06-25 17:08:53 -0400

    updated files

--- .history/mclareact/src/App_20200625165810.js
+++ .history/mclareact/src/App_20200625165810.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./components";
 
 class App extends Component {
   render() {
