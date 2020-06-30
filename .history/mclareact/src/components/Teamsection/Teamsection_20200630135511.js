c8562621a85ec82ea6da3371a2453aa9aaa03ec5
Ky Kyler <ky.kyler1@gmail.com>
2020-06-30 15:48:59 -0400

    updated score and collection box

--- .history/mclareact/src/components/Teamsection/Teamsection_20200630135511.js
+++ .history/mclareact/src/components/Teamsection/Teamsection_20200630135511.js
@@ -7,7 +7,7 @@ import image3 from "../../pictures/john-cameron-qjATQpoeK5Y-unsplash.jpg";
 function Teamsection(params) {
   return (
     <div className="sectionTeam section-team text-center">
-      <div className="containerTeam">
+      <div className="containerTEam">
         <h1 className="titleTeam">Meet the Team</h1>
         <div className="teamTeam">
           <div className="rowTM">
