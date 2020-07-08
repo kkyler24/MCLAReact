a5884d317b4e3fadd6543dbe4384a2056716af78
Ky Kyler <ky.kyler1@gmail.com>
2020-07-08 18:02:45 -0400

    updated heroku

--- .history/mclareact/Client/src/App_20200707234323.js
+++ .history/mclareact/Client/src/App_20200707234323.js
@@ -12,7 +12,7 @@ class App extends Component {
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/About" component={AboutUS} />
+            <Route path="/Aboutus" component={AboutUS} />
 
             {/* <Route path="/About" render={(props) => <AboutUS {...props} />} /> */}
 
