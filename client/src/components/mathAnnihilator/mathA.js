3b2cf601bba2231545b55f7e5b302afc0af56124
Ky Kyler <ky.kyler1@gmail.com>
2020-07-13 18:04:29 -0400

    created math js

---
+++ client/src/components/mathAnnihilator/mathA.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./mathA.module.css";
+import Spacebackground from "../../Assets/spaceship/Spacebackground.png";
+
+const spacepic = {
+  background: "spacebackground",
+  repeat: "no repeat",
+};
+function mathA() {
+  return <div></div>;
+}
+export default mathA;
