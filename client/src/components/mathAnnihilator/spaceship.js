c6b98a69b027ab98e082d4e77e0e979537aa921e
Ky Kyler <ky.kyler1@gmail.com>
2020-07-17 00:06:23 -0400

    updated movement

--- client/src/components/mathAnnihilator/spaceship.js
+++ client/src/components/mathAnnihilator/spaceship.js
@@ -5,6 +5,11 @@ import Shipspacepic from "../../pictures/MathApics/spaceship/spaceship.png";
 export default class SpaceShip extends Component {
   constructor(el, props) {
     super(props);
+    this.shipFunction = this.shipFunction.bind(this);
+    this.state = {
+      left: 0,
+      top: 0,
+    };
     this.x = 840 / 2;
     this.y = -350;
     this.move = function (dir) {
@@ -14,21 +19,40 @@ export default class SpaceShip extends Component {
       };
     };
 
-    // input.addEventListner("keypressed", (event) => {
-    //   if (event.keyCode === Arrow_right) {
-    //     return;
-    //   }
-    //   SpaceShip.move();
-    // });
-    // this.ship = () => {
-    //   return function keyPressed() {
-    //     if (keyCode === ArrowRight) {
-    //       ship.move(1);
-    //     } else if (keyCode === LeftArrow) {
-    //       ship.move(-1);
+    // move(x, y) {
+    //     	this.setState({
+    //       	left: this.state.left + x,
+    //       	top: this.state.right -x,
+    //       });
     //     }
+
+    //   SpaceShip.move();
+
+    //   this.ship = () => {
+    //     return function shipFunction(event){
+    //   if(event.keyCode === ArrowRight) {
+    //     // Key onClick={e => this.props.move(-10, 0)}
+    //     //Do whatever when leftarrow is pressed
     //   };
+    //    else if (keyCode === LeftArrow) {
+    //         ship.move(-1);
+    // }
     // };
+
+    // }
+
+    // componentDidMount(){
+    //   document.addEventListener("keydown", this.shipFunction, false);
+    // };
+    // componentWillUnmount(){
+    //   document.removeEventListener("keydown", this.shipFunction, false);
+
+    //     //
+    //     // <button onClick={e => this.props.move(10, 0)}>Right</button>
+    //     // <button onClick={e => this.props.move(0, -10)}>Up</button>
+    //     // <button onClick={e => this.props.move(0, 10)}>Down</button>
+    //   //       ship.move(1);
+    //   //
   }
   render() {
     return (
@@ -38,3 +62,11 @@ export default class SpaceShip extends Component {
     );
   }
 }
+
+// 	render() {
+//   	return <div>
+//     	<Thing left={this.state.left} top={this.state.top}/>
+//       <Controls move={(x, y) => this.move(x, y)}/>
+//     </div>;
+//   }
+// }
