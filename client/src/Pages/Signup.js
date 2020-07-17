d8c773424325a1a3008688123dc025762536205a
Ky Kyler <ky.kyler1@gmail.com>
2020-07-17 18:35:20 -0400

    adjusted container

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -4,20 +4,27 @@ import Login from "../../src/components/Login/Login.js";
 // import Image1 from "../../pictures/controller.jpg";
 // import Footer from "../components/footerSocial";
 
-// const background = {
-//   backgroundImage: `url(${Image1})`,
-//   backgroundSize: "cover",
-//   backgroundPosition: "top center",
-//   minHeight: "700px",
+// const Style = {
+//   display: "flex",
+// // flexdirection: "row",
+// .container {
+//   display: flex,
+//   align-items: center, // Align both children horizontally
+//   justify-content: spaced-evenly, // Give both elements even space
+//  }
+//   // backgroundImage: `url(${Image1})`,
+//   // backgroundSize: "cover",
+//   // backgroundPosition: "top center",
+//   // minHeight: "700px",
 // };
 function SignupP() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col md-5">
+        <div className="col md-6">
           <SignupC />
         </div>
-        <div className="col md-5">
+        <div className="col md-6">
           <Login />
         </div>
       </div>
