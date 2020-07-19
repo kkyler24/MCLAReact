688e8bf34ffc65386d524e97b08fe0265856a10c
Ky Kyler <ky.kyler1@gmail.com>
2020-07-19 15:17:32 -0400

    commented out math Invader

--- client/src/components/mathAnnihilator/Aliens.js
+++ client/src/components/mathAnnihilator/Aliens.js
@@ -1,55 +1,55 @@
-import React from "react";
-import Green from "../../pictures/MathApics/invaders_png/green.png";
-import Yellow from "../../pictures/MathApics/invaders_png/yellow.png";
-import Red from "../../pictures/MathApics/invaders_png/red.png";
+// import React from "react";
+// import Green from "../../pictures/MathApics/invaders_png/green.png";
+// import Yellow from "../../pictures/MathApics/invaders_png/yellow.png";
+// import Red from "../../pictures/MathApics/invaders_png/red.png";
 
-const aliensImagesArray = [Green, Yellow, Red];
+// const aliensImagesArray = [Green, Yellow, Red];
 
-function AlienComponent(props) {
-  console.log(props.canvasRef);
-  // if (props.canvasRef) {
+// function AlienComponent(props) {
+//   console.log(props.canvasRef);
+//   // if (props.canvasRef) {
 
-  // }
-  // return <></>;
-  return (
-    <div
-      style={{
-        position: "absolute",
-        top: props.position[1],
-        left: props.position[0],
-        height: "90px",
-        width: "90px",
-        zIndex: 2,
-      }}
-    >
-      <img src={props.image} alt="Alien" style={{ width: 90, height: 90 }} />
-    </div>
-  );
-}
+//   // }
+//   // return <></>;
+//   return (
+//     <div
+//       style={{
+//         position: "absolute",
+//         top: props.position[1],
+//         left: props.position[0],
+//         height: "90px",
+//         width: "90px",
+//         zIndex: 2,
+//       }}
+//     >
+//       <img src={props.image} alt="Alien" style={{ width: 90, height: 90 }} />
+//     </div>
+//   );
+// }
 
-function Aliens(props) {
-  //   const alienInterval =
-  return (
-    <div>
-      <AlienComponent
-        // canvasRef={props.canvasRef.current}
-        position={[0, 0]}
-        image={aliensImagesArray[0]}
-        pointVaule={10}
-      />
-      <AlienComponent
-        // canvasRef={props.canvasRef.current}
-        position={[40, 0]}
-        image={aliensImagesArray[1]}
-        pointVaule={10}
-      />
-      <AlienComponent
-        // canvasRef={props.canvasRef.current}
-        position={[80, 0]}
-        image={aliensImagesArray[2]}
-        pointVaule={10}
-      />
-    </div>
-  );
-}
-export default Aliens;
+// function Aliens(props) {
+//   //   const alienInterval =
+//   return (
+//     <div>
+//       <AlienComponent
+//         // canvasRef={props.canvasRef.current}
+//         position={[0, 0]}
+//         image={aliensImagesArray[0]}
+//         pointVaule={10}
+//       />
+//       <AlienComponent
+//         // canvasRef={props.canvasRef.current}
+//         position={[40, 0]}
+//         image={aliensImagesArray[1]}
+//         pointVaule={10}
+//       />
+//       <AlienComponent
+//         // canvasRef={props.canvasRef.current}
+//         position={[80, 0]}
+//         image={aliensImagesArray[2]}
+//         pointVaule={10}
+//       />
+//     </div>
+//   );
+// }
+// export default Aliens;
--- client/src/components/mathAnnihilator/mathgamepage.js
+++ client/src/components/mathAnnihilator/mathgamepage.js
@@ -1,35 +1,35 @@
-import React, { Component } from "react";
-import style from "./mathA.module.css";
-import Spacebackground from "../../pictures/MathApics/spaceship/cosmicgalaxbluebackground.jpg";
-import MathInvaders from "../mathAnnihilator/mathInvaderConponent.js";
+// import React, { Component } from "react";
+// import style from "./mathA.module.css";
+// import Spacebackground from "../../pictures/MathApics/spaceship/cosmicgalaxbluebackground.jpg";
+// import MathInvaders from "../mathAnnihilator/mathInvaderConponent.js";
 
-const spaceGameIndex = {
-  backgroundImage: `url(${Spacebackground})`,
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat",
-  width: "100%",
-  // height: "800px",
-};
+// const spaceGameIndex = {
+//   backgroundImage: `url(${Spacebackground})`,
+//   backgroundSize: "cover",
+//   backgroundRepeat: "no-repeat",
+//   width: "100%",
+//   // height: "800px",
+// };
 
-function mathA() {
-  return (
-    <div
-      className={`${style.section} ${style.textCenter} ${style.background} ${style.containerFluid}`}
-      style={spaceGameIndex}
-    >
-      <div className={style.Container}>
-        <h2 className={style.title}>Math Invader</h2>
-        <p className={style.description}></p>
+// function mathA() {
+//   return (
+//     <div
+//       className={`${style.section} ${style.textCenter} ${style.background} ${style.containerFluid}`}
+//       style={spaceGameIndex}
+//     >
+//       <div className={style.Container}>
+//         <h2 className={style.title}>Math Invader</h2>
+//         <p className={style.description}></p>
 
-        <div className={style.row}>
-          <div id="canvas" className={`${style.textCenter} ${style.btnroundC}`}>
-            <div className={style.containerFluid}>
-              <MathInvaders />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-export default mathA;
+//         <div className={style.row}>
+//           <div id="canvas" className={`${style.textCenter} ${style.btnroundC}`}>
+//             <div className={style.containerFluid}>
+//               <MathInvaders />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default mathA;
