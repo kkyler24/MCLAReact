3696817722f6fe09c83eee3295604d64e4abcf33
Ky Kyler <ky.kyler1@gmail.com>
2020-09-08 22:20:34 -0400

    social update

--- client/src/components/SocialFooter/SocialFooter.js
+++ client/src/components/SocialFooter/SocialFooter.js
@@ -8,7 +8,7 @@ function SocialFooter(props) {
   return (
     <Row id="rowfooter">
       <div id="Spacer"></div>
-      <div id="sm-12SocialFooter">
+      {/* <div id="sm-12SocialFooter"> */}
         <div className="card social-footer">
           <h5 id="H5text">Contact Us</h5>
           <div className="cardBody">
